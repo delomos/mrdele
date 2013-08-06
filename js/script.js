b014dbebf47acc03c7ad4c284863fe863c5988fe
ramitissmart <ramitissmart@gmail.com>
2013-08-06 10:23:33 +0530

    files uploaded
    initial design

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+
+$(document).ready(function ()
+                  {
+
+	window.ddzSlider = $('.home_slider').ddzSlider({animSpeed:800,easing:"swing",speed:10});
+	window.ddzSlider = $('.grid_gall_1').ddzSlider({animSpeed:800,easing:"linear",next:"#slider2Next_1",prev:"#slider2Back_1", speed:7, auto:false});
+		window.ddzSlider = $('.grid_gall_2').ddzSlider({animSpeed:800,easing:"linear",next:"#slider2Next_2",prev:"#slider2Back_2", speed:7, auto:false});
+			window.ddzSlider = $('.grid_gall_3').ddzSlider({animSpeed:800,easing:"linear",next:"#slider2Next_3",prev:"#slider2Back_3", speed:7, auto:false});
+	 
+});
+
+
+
